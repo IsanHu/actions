9cf91f8b0b6f5b774adab541b920681c81e90f0a
Isan <3isan333@gmail.com>
2020-09-03 17:03:15 +0800

    no message

--- .github/scripts/upload_pgy.sh
+++ .github/scripts/upload_pgy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -eo pipefail
-result=`curl -F 'file=@'$PWD'/build/heishi-ios.ipa' -F '_api_key=$PGY_API_KEY' https://www.pgyer.com/apiv2/app/upload`
+echo "====="
+echo $PGY_API_KEY
+echo "====="
+result=`curl -F 'file=@'$PWD'/build/heishi-ios.ipa' -F "_api_key=$PGY_API_KEY" https://www.pgyer.com/apiv2/app/upload`
 echo $result
 
