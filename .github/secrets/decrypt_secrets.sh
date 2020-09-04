6f2d005a092292e12d24a06d425e962c94b2c92d
Isan <3isan333@gmail.com>
2020-09-04 17:07:40 +0800

    no message

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 echo "$IOS_KEYS"
 echo "ddd1"
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Heishi_AppStore.mobileprovision ./.github/secrets/Heishi_AppStore.mobileprovision.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Heishi_AppStore.mobileprovision ./.github/secrets/Heishi_AppStore.mobileprovision.gpg
 echo "ddd2"
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/HeiShi.p12 ./.github/secrets/HeiShi.p12.gpg
 echo "ddd3"
@@ -10,8 +10,8 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "ddd4"
 cp ./.github/secrets/Heishi_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Heishi_AppStore.mobileprovision
 ls -al ~/Library/MobileDevice/Provisioning\ Profiles
-chmod 777 ~/Library/MobileDevice/Provisioning\ Profiles/Heishi_AppStore.mobileprovision
-ls -al ~/Library/MobileDevice/Provisioning\ Profiles
+# chmod 777 ~/Library/MobileDevice/Provisioning\ Profiles/Heishi_AppStore.mobileprovision
+# ls -al ~/Library/MobileDevice/Provisioning\ Profiles
 echo "ddd5"
 
 security create-keychain -p "" build.keychain
