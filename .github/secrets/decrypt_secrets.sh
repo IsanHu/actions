f10fb3aea7ada2bd553a9705a02c5cd3ee761e38
Isan <3isan333@gmail.com>
2020-09-04 17:15:59 +0800

    no message

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -8,7 +8,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/
 echo "ddd3"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "ddd4"
-cp ./.github/secrets/Heishi_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Heishi_AppStore.mobileprovision
+cp ./.github/secrets/profile_adhoc.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/profile_adhoc.mobileprovision
 ls -al ~/Library/MobileDevice/Provisioning\ Profiles
 # chmod 777 ~/Library/MobileDevice/Provisioning\ Profiles/Heishi_AppStore.mobileprovision
 # ls -al ~/Library/MobileDevice/Provisioning\ Profiles
